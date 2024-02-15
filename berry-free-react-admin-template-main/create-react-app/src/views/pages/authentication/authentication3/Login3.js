f37fadd6d42c68235313cc4a992451e282c7f66c
Linuka Lolitha Jayesinghe <99358426+it21274398@users.noreply.github.com>
2024-02-15 09:17:46 +0530

    add a comment

--- berry-free-react-admin-template-main/create-react-app/src/views/pages/authentication/authentication3/Login3.js
+++ berry-free-react-admin-template-main/create-react-app/src/views/pages/authentication/authentication3/Login3.js
@@ -10,7 +10,7 @@ import AuthCardWrapper from '../AuthCardWrapper';
 import AuthLogin from '../auth-forms/AuthLogin';
 import Logo from 'ui-component/Logo';
 import AuthFooter from 'ui-component/cards/AuthFooter';
-
+//------------------------------------------------------------------------------------------
 // assets
 
 // ================================|| AUTH3 - LOGIN ||================================ //
