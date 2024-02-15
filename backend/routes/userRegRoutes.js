10fe995fe3a21781e4c9c77f47258c6eb8062a38
Linuka Lolitha Jayesinghe <99358426+it21274398@users.noreply.github.com>
2024-02-15 10:00:05 +0530

    backend deleted

--- backend/routes/userRegRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userRegController = require("../controllers/userRegController");
-
-router.post("/", userRegController.createUser);
-
-module.exports = router;
